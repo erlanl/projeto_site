3670e158c19950d952733a47c0bca82d83992a30
Erlan Lira <erlanlirajr@gmail.com>
2021-11-11 10:44:57 -0300

    upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function mudarValor()
+{
+ if(document.getElementById("botao_contato").value == "Contato")
+ {
+  document.getElementById("botao_contato").value = "erlanlirajr@gmail.com";
+ } 
+ else
+ {
+  document.getElementById("botao_contato").value = "Contato";
+ }
+}
